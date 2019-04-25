9288d26e74bc85e97038bdf7b4be74f00f4e6c2b
chengpeng jiang <chengpengjiang@Jay.local>
2019-04-25 14:36:46 +0200

    error+dealing

--- src/go_code/error_deal/main.go
+++ src/go_code/error_deal/main.go
@@ -5,6 +5,16 @@ import (
 )
 
 func test() {
+	defer func() {
+		//recover 是一个内置函数可以捕获到异常
+		if err := recover(); err != nil {
+			//说明捕获到错误
+			fmt.Println("err=,", err)
+			//发送邮件给某某管理员
+			fmt.Println("管理员：错误出现了")
+		}
+	}()
+
 	num1 := 100
 	num2 := 0
 	res := num1 / num2
@@ -15,6 +25,6 @@ func main() {
 	//  1. 即使错了，仍然继续处理下面
 	//	2. 错误预警机制
 	test()
-
 	fmt.Println("main()下面的代码：")
+
 }
